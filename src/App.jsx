f6b096acaf2e46fa27b3c7532725f7e4ada3cda4
Henry Ben <benhenry088@gmail.com>
2024-08-12 14:52:21 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css'
+import Todolist from './components/TodoList';
+
+function App() {
+  
+
+  return (
+   
+      <div className='todo-app'>
+
+      <Todolist />
+
+         
+      </div>
+      
+    
+  )
+}
+
+export default App
